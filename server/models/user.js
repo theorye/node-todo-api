f82f4f8d6caaa9545d1c0aeab8b6d2d89c9b0d83
Adrian H <limitmaker@Adrians-MacBook-Air.local>
2017-04-24 10:02:58 -0500

    Add POST /users"

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,89 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User', {
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minLength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: ( value => validator.isEmail(value)),
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+  // console.log(token);
+  return user.save().then(() => {
+    // console.log(token);
+    return token;
+  }).catch( err => {
+    console.log(err);
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
+// just model:
+// var User = mongoose.model('User', {
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     minLength: 1,
+//     unique: true,
+//     validate: {
+//       validator: validator.isEmail,
+//       message: `$value is not a valid email`,
+//     }
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 6
+//   },
+//   tokens: [{
+//     access: {
+//       type: String,
+//       required: true
+//     },
+//     token: {
+//       type: String,
+//       required: true
+//     }
+//   }]
+// });
 
 module.exports = {
   User,
--- server/server.js
+++ server/server.js
@@ -95,6 +95,22 @@ app.delete('/todos/:id', (req, res) => {
   })
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    // console.log(token);
+    // console.log(user);
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
